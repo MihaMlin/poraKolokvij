77fb4d021cf89ccb7bfa9b9ad56708fd96ebabe7
Miha Mlinaric <mlinmiha@gmail.com>
2023-12-14 18:53:16 +0100

    added MPAndroidChart to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/mihamlinaric/porakolokvij/MainActivity.kt
+++ app/src/main/java/com/mihamlinaric/porakolokvij/MainActivity.kt
@@ -3,10 +3,9 @@ package com.mihamlinaric.porakolokvij
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.mihamlinaric.porakolokvij.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +17,6 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigationView = binding.bottomNavigationView
         val navController = findNavController(R.id.fragmentContainer)
-
-        //val appBarConfiguration = AppBarConfiguration(setOf(R.id.pieChartFragment, R.id.lineChartFragment))
-        //setupActionBarWithNavController(navController, appBarConfiguration)
 
         bottomNavigationView.setupWithNavController(navController)
     }
